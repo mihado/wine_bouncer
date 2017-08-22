26252f46f4e0bcf580e7847440d7ae6bba1f4b98
Minh Ha <mihado@minh.im>
2017-08-22 18:44:28 +1200

    try using grape 1.0.0; run SimpleCov instead of CodeClimate::TestReporter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,8 +16,10 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
+
+# https://github.com/codeclimate/ruby-test-reporter/blob/master/CHANGELOG.md#v100-2016-11-03
+require 'simplecov'
+SimpleCov.start
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
